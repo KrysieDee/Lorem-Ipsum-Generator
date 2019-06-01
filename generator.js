35196d14504d52be930a9b76c656fbf3bcc8fb39
KrysieDee <krystal.smithx@gmail.com>
2019-05-31 22:57:13 -0400

    rearranged the quotes

--- generator.js
+++ generator.js
@@ -8,10 +8,21 @@ function GenerateNewText() {
   // Add property to the object
   this.sentences =
     [
-      "Awesome quote number 1.",
-      "Second awesome quote.",
-      "Yet another great quote.",
-      "Ok, last quote."
+      "When you play the game of thrones, you win or you die. There is no middle ground.",
+      "Winter is coming.",
+      "You know nothing, Jon Snow.",
+      "I dont plan on knitting by the fire while men fight for me. I might be small, Lord Glover, and I might be a girl, but I am every bit as much a Northerner as you… and I dont need your permission to defend the North.",
+      "Tell Cersei. I want her to know it was me.",
+      "Chaos isn’t a pit. Chaos is a ladder.",
+      "The man who passes the sentence should swing the sword.",
+      "Yes. All men must die, but we are not men.",
+      "Thats what I do: I drink and I know things.",
+      "Youre going to die tomorrow, Lord Bolton. Sleep well.",
+      "If you think this has a happy ending, you havent been paying attention.",
+      "There is only one thing we say to death: Not today.",
+      "The things I do for love.",
+      "Any man who must say, ‘I am the king,’ is no true king. I’ll make sure you understand that when Ive won your war for you.",
+      "Never forget what you are. The rest of the world will not. Wear it like armor, and it can never be used to hurt you."
    ];
 }
 
